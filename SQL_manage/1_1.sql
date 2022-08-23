72f971dc90c3ab3bdc2fd7b2d97cb77fd821cbdc
TakuSe <49015441+TakumiSeo@users.noreply.github.com>
2022-08-24 03:38:25 +0900

    Add files via upload

---
+++ SQL_manage/1_1.sql
@@ -0,0 +1,29 @@
+db.grades.insert(
+
+	{	
+"student_id" : 50,
+"class_id": 28,
+"scores":[
+		{"type" : "exam", "score": 58.6},
+		{"type" : "quiz", "score": 22.4},
+		{"type" : "homework", "score": 74.2},
+		{"type" : "homework", "score": 68.3},
+		{"type" : "homework", "score": 17.9},
+		]
+	},
+	
+		{	
+"student_id" : 50,
+"class_id": 29,
+"scores":[
+		{"type" : "exam", "score": 58.8},
+		{"type" : "quiz", "score": 22.4},
+		{"type" : "homework", "score": 74.2},
+		{"type" : "homework", "score": 68.3},
+		{"type" : "homework", "score": 17.9},
+		]
+	}
+	
+)
+
+
